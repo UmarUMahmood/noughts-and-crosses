7e0b72a5f9fe505801afb92b8692a99e9fee6465
UmarUMahmood <umar.mahmood@infinityworks.com>
2021-09-30 17:42:47 +0100

    unit test for resetGame function

--- academy.test.js
+++ academy.test.js
@@ -1,15 +1,38 @@
-const { getBoard } = require("./academy")
+const { getBoard, resetGame } = require("./academy")
 
 describe("When getBoard is called", () => {
     test("We are returned with a 2D array that represents our board", () => {
 
-        jest.spyOn(console, 'log').mockImplementation(() => { });
+        jest.spyOn(console, 'log').mockImplementation(() => { })
 
         const state = { board: [["nought", "nought", null], ["cross", "cross", null], [null, null, null]] }
 
         const expected = [["nought", "nought", null], ["cross", "cross", null], [null, null, null]]
 
         const actual = getBoard(state)
+
+        expect(actual).toStrictEqual(expected)
+    })
+})
+
+describe("When resetBoard is called", () => {
+    test("The state variables are reset to their initial values", () => {
+
+        jest.spyOn(console, 'log').mockImplementation(() => { });
+
+        const state = {
+            board: [["nought", "nought", "nought"], ["cross", "cross", null], [null, null, null]],
+            turnCount: 5,
+            winner: "nought"
+        }
+
+        const expected = {
+            board: [[null, null, null], [null, null, null], [null, null, null]],
+            turnCount: 0,
+            winner: null
+        }
+
+        const actual = resetGame(state)
 
         expect(actual).toStrictEqual(expected)
     })
